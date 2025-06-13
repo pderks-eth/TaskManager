a1b650304ad1190395cf5fc6b949faa5821c33bc
Philipp <philipp.derks@lernende.ethz.ch>
2025-06-13 16:14:53 +0200

    inital commit

---
+++ src/test/java/ch/bbw/TaskManager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.TaskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
