ae2f6cff364f303919a1235c3ccdb388e3ba5090
mfedorchenko <mfedorchenko@ethz.ch>
2025-06-17 16:52:43 +0200

    bug fix: app is running

--- src/main/java/ch/bbw/taskmanager/TaskManagerApplication.java
+++ src/main/java/ch/bbw/taskmanager/TaskManagerApplication.java
@@ -3,9 +3,12 @@ package ch.bbw.taskmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 @EntityScan(basePackages = "ch.bbw.taskmanager.model")
+@EnableJpaRepositories(basePackages = "ch.bbw.taskmanager.repository")
 public class TaskManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
