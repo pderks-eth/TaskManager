defab6532b597d64599b60de1f91b726c6561955
mfedorchenko <mfedorchenko@ethz.ch>
2025-06-17 11:19:07 +0200

    feat: add findByCompleted method to TaskRepository

--- src/main/java/ch/bbw/TaskManager/repository/TaskRepository.java
+++ src/main/java/ch/bbw/TaskManager/repository/TaskRepository.java
@@ -1,5 +1,12 @@
 package ch.bbw.TaskManager.repository;
 
-public class TaskRepository {
-    
+import ch.bbw.TaskManager.model.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task, Long> {
+    List<Task> findByCompleted(boolean completed); 
 }
